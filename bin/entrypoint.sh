f4d7ef55f3483b39080dc30016edd29e211fca7e
Sandeep Jadoonanan <jsanweb@gmail.com>
2020-02-18 23:10:48 -0500

    Remove the bin dir. Format docker-compose.yml file.

--- bin/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Run the application with uvicorn.
-if [ "$ENV" = "dev" ]; then
-    uvicorn --host 0.0.0.0 --port 8000 --reload --reload-dir=/app/markette markette.app:app
-else
-    uvicorn --host 0.0.0.0 --port 8000 --workers 2 markette.app:app
-fi
